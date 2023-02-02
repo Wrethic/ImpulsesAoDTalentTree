e41acf2bcede93381bafce933e4101df779cc421
Wrethic/Desktop <bigjmills90@Gmail.com>
2023-02-02 17:54:01 -0600

    UI First Alpha

--- Talent Tree/resources/js/talentcalc.js
+++ Talent Tree/resources/js/talentcalc.js
@@ -4,4 +4,22 @@ $(document).ready(function() {
         $cols.removeClass(addclass);
         $(this).addClass(addclass);
     });
+    var talents  = document.getElementsByClassName('aod-talent-tree-talent');
+    var images_urls = [];
+    var images_names = [];
+    var tmp;
+    for(var i=0;i < talents.length;i++){
+      images_urls[i] = talents[i].getAttribute("href"); 
+      tmp = talents[i].getAttribute('src').split('/');
+      images_names[i] = tmp[tmp.length -1].split['.'][0];
+    }
+
+    console.log(talents); // ["imageOne", "imageTwo", "imageThree"]
+
+});
+
+$('.aod-talent-tree-talent').hover(function () {
+    $('#slayer').stop().fadeIn();
+}, function () {
+    $('#slayer').stop().fadeOut();
 });
