7b2b69fe8a741d2920d09166b2caa0ba3e9c056d
SpeedyDevin <livingfit90@gmail.com>
2023-02-02 22:51:00 -0600

    Computer Swap - Playing with talent information array.

--- Talent Tree/resources/js/talentcalc.js
+++ Talent Tree/resources/js/talentcalc.js
@@ -1,25 +1,25 @@
+var talents  = document.getElementsByClassName('aod-talent-tree-talent');
+
 $(document).ready(function() {
     var addclass = 'color';
-    var $cols = $('.aod-talent-tree-talent-inner').click(function(e) {
+    var $cols = $('.aod-talent-tree-talent').click(function(e) {
         $cols.removeClass(addclass);
         $(this).addClass(addclass);
     });
-    var talents  = document.getElementsByClassName('aod-talent-tree-talent');
     var images_urls = [];
     var images_names = [];
+    var talent_points = [];
     var tmp;
     for(var i=0;i < talents.length;i++){
-      images_urls[i] = talents[i].getAttribute("href"); 
-      tmp = talents[i].getAttribute('src').split('/');
-      images_names[i] = tmp[tmp.length -1].split['.'][0];
-    }
 
-    console.log(talents); // ["imageOne", "imageTwo", "imageThree"]
+    }
 
 });
 
 $('.aod-talent-tree-talent').hover(function () {
-    $('#slayer').stop().fadeIn();
+    $('#slayer1').stop().fadeIn();
 }, function () {
-    $('#slayer').stop().fadeOut();
+    $('#slayer1').stop().fadeOut();
+
+    console.log(talents); // ["imageOne", "imageTwo", "imageThree"]
 });
