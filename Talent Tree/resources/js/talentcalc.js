87f13a2ef2c55cef5e85f81d45b6d451a1ee1174
SpeedyDevin <livingfit90@gmail.com>
2023-02-04 22:28:44 -0600

    Prepping output

--- Talent Tree/resources/js/talentcalc.js
+++ Talent Tree/resources/js/talentcalc.js
@@ -11,30 +11,50 @@ $(document).ready(function() {
     for(var i=0;i < talents.length;i++){
         point_display[i] = point_display[i].textContent = talent_points[i].toString() + "/" + talent_points_max[i].toString();
     }
-
-    var e = $('.aod-talent-tree-talent').click(function(e) {
-        console.log()
-        if(this.getAttribute('data-cell') == 1 && talent_points[0] < talent_points_max[0]) {
-            var test = document.getElementById('slayer');
-            console.log(test);
-            used_points++;
-            talent_points[0] = talent_points[0] + 1;
-            test = test.textContent = talent_points[0].toString() + "/" + talent_points_max[0].toString();
-        }
-
-    window.oncontextmenu = (e) => {
-      e.preventDefault()
-        if(this.getAttribute('data-cell') == 1 && talent_points[0] > 0) {
-            var test = document.getElementById('slayer');
-            console.log(test);
+window.addEventListener('contextmenu', (ev) => {
+        ev.preventDefault();
+        ev = ev.target.parentNode.parentNode.parentNode;
+        const k = ev.getAttribute('data-cell')-1;
+        const pointText = ev.nextElementSibling;
+        const remaining_points = total_max_points - used_points;
+        if(talent_points[k] > 0 && remaining_points < total_max_points) {
             used_points--;
-            talent_points[0] = talent_points[0] - 1;
-            test = test.textContent = talent_points[0].toString() + "/" + talent_points_max[0].toString();
-            console.log(used_points);
-            console.log(total_max_points);
+            talent_points[k] = talent_points[k] - 1;
+            const textUpdate = pointText.textContent = talent_points[k].toString() + "/" + talent_points_max[k].toString();
+            descUpdate(ev, k);
             talentC();
         }
-    }
+    });
+    var e = $('.aod-talent-tree-talent').click(function(e) {
+        const k = this.getAttribute('data-cell')-1;
+        const pointText = this.nextElementSibling;
+        const remaining_points = total_max_points - used_points;
+        var n = [0];
+        e.preventDefault();
+
+        if(talent_points[k] < talent_points_max[k] && remaining_points > 0 && this.getAttribute('data-enabled') === "true") {
+            used_points++;
+            talent_points[k] = talent_points[k] + 1;
+            const textUpdate = pointText.textContent = talent_points[k].toString() + "/" + talent_points_max[k].toString();
+            descUpdate(this, k);
+        }
+        if (talent_points[k] == talent_points_max[k]) {
+            const from = this.getAttribute('data-cell')
+            const connector = document.getElementsByClassName('aod-talent-tree-connection');
+            for(i=0; i < connector.length;i++){
+                if (from == connector[i].getAttribute('data-from-cell')) {
+                    n[i] = connector[i].getAttribute('data-to-cell');
+                }
+            }
+            const next = document.getElementsByClassName('aod-talent-tree-talent');
+            for(i=0; i < next.length;i++){
+                for(z=0; z < n.length;z++){
+                    if (n[z] == next[i].getAttribute('data-cell')){
+                        next[i].setAttribute('data-enabled','true');
+                    }
+                }
+            }
+        }
         
     talentC();
     });
@@ -55,17 +75,53 @@ function listQ(){
 
 $('.aod-talent-tree-talent').hover(function () {
     var talentHover = this.getAttribute('data-cell')-1;
-    var name = this.id.toString();
+    var name = this.id.toString().replace(/-/g, ' ');;
     var desc = data[name][talent_points[talentHover]].description;
-    console.log(desc);
     var overlay = document.getElementById('talent-description');
-    var title = document.getElementById('talent-title').textContent = name.charAt(0).toUpperCase() + name.slice(1)
+    var title = name.charAt(0).toUpperCase() + name.slice(1);
+    const words = title.split(" ");
+
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    }
+    document.getElementById('talent-title').textContent = words.join(" ");
+
     var tooltip = document.getElementById('talent-descr').textContent = desc;
 
     $(overlay).stop().fadeIn();
-}, function () {
-    $(document.getElementById('talent-description')).stop().fadeOut();
 });
+
+function descUpdate(e, k){
+    var name = e.id.toString().replace(/-/g, ' ');;
+    var desc = data[name][talent_points[k]].description;
+    var overlay = document.getElementById('talent-description');
+    var title = name.charAt(0).toUpperCase() + name.slice(1);
+    const words = title.split(" ");
+
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    }
+    document.getElementById('talent-title').textContent = words.join(" ");
+
+    var tooltip = document.getElementById('talent-descr').textContent = desc;    
+}
+
+var myTableArray = [];
+tableData();
+
+function tableData() {
+    var table = document.getElementById('results');
+    console.log(table.rows[0].children);
+    $("table#results tr").each(function() {
+        var arrayOfThisRow = [];
+        var tableData = $(this).find('td');
+        if (tableData.length > 0) {
+            tableData.each(function() { arrayOfThisRow.push($(this).text()); });
+            myTableArray.push(arrayOfThisRow);
+        }
+        console.log(myTableArray);
+    });
+}
 
 $('input').click(function(){
     var soul_value;
