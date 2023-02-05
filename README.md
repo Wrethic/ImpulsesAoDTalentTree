fceb8147483f615749b80e04a1810c297e07ee7b
SpeedyDevin <livingfit90@gmail.com>
2023-02-05 03:57:50 -0600

    Beta Release 0.8b

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ImpulsesAoDTalentTree
-Just a generic HTML/JS talent tree for AoD RPG
+# Use Instructions #
+
+1. Download Latest .zip File
+2. Extract to any location on your PC.
+3. Open index.html using your web browser of choice.
+	1. index.html is located in the Talent Tree folder.
+4. Report any bugs, text imperfections, or feature requests.
--- Talent Tree/resources/js/talentcalc.js
+++ Talent Tree/resources/js/talentcalc.js
@@ -4,7 +4,7 @@ var talent_points_max = [5,5,5,5,5,5,5,5,5,1,5,5,5,1,3,5,1,1,1,1,1,1,1,1,3,1,1,3
 var soul_level = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 var total_max_points = 0;
 var used_points = 0;
-var table = document.getElementById('results').rows;
+var table = document.getElementById('results');
 
 $(document).ready(function() {
     var point_display = document.getElementsByClassName('aod-talent-tree-talent-points');
@@ -18,12 +18,29 @@ window.addEventListener('contextmenu', (ev) => {
         const k = ev.getAttribute('data-cell')-1;
         const pointText = ev.nextElementSibling;
         const remaining_points = total_max_points - used_points;
-        if(talent_points[k] > 0 && remaining_points < total_max_points) {
+        if(talent_points[k] > 0 && remaining_points < total_max_points && ev.getAttribute('data-enabled') === "true") {
             used_points--;
             talent_points[k] = talent_points[k] - 1;
             const textUpdate = pointText.textContent = talent_points[k].toString() + "/" + talent_points_max[k].toString();
             descUpdate(ev, k);
             talentC();
+        }
+        if (talent_points[k] == 0) {
+            const from = ev.getAttribute('data-cell')
+            const connector = document.getElementsByClassName('aod-talent-tree-connection');
+            for(i=0; i < connector.length;i++){
+                if (from == connector[i].getAttribute('data-to-cell')) {
+                    var n = connector[i].getAttribute('data-from-cell');
+                    break;
+                }
+            }
+            var z = document.getElementsByClassName('aod-talent-tree-talent');
+            for(i=0; i < z.length;i++) {
+                if (z[i].getAttribute('data-cell') == 1) {
+                    z[i].setAttribute('data-enabled','true');
+                    break;
+                }
+            }
         }
     });
     var e = $('.aod-talent-tree-talent').click(function(e) {
@@ -55,6 +72,7 @@ window.addEventListener('contextmenu', (ev) => {
                     }
                 }
             }
+            this.setAttribute('data-enabled','false');
         }
         
     talentC();
@@ -104,24 +122,29 @@ function descUpdate(e, k){
     }
     title = document.getElementById('talent-title');
     title.textContent = words.join(" ");
-    tableData(title)
+    title = words.join(" ");
+    tableData(title, name, k);
 
     var tooltip = document.getElementById('talent-descr').textContent = desc;    
 }
 
-function tableData(e) {
-    var table = document.getElementById('results');
-    console.log(table.rows.length);
+function tableData(e, k, x) {
     for(count=1;count < table.rows.length;count++){
-        console.log(table.rows[count].children[0].innerHTML);
         if(table.rows[count].children[0].innerHTML == "" ){
-            var newRow = count;
-            console.log(newRow);
+            table.rows[count].children[0].textContent = e;
+            table.rows[count].children[1].textContent = data[k][talent_points[x]].description;
             break;
         }
-        else if (table[count].children[0].innerText === e.innerText) {
-            var newRow = count;
-
+        else if (table.rows[count].children[0].innerHTML === e) {
+            if (talent_points[x] == 0)
+            {
+                table.rows[count].children[0].textContent = "";
+                table.rows[count].children[1].textContent = ""; 
+            }
+            else {
+                table.rows[count].children[0].textContent = e;
+                table.rows[count].children[1].textContent = data[k][talent_points[x]].description;
+            }
             break;
         }
     }
